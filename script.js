66ca607ddbfdeb6450ac4c5d94e5d8722d03e3d8
Gustavo Mattos <gustavomattos777@gmail.com>
2022-08-02 10:19:08 -0300

    correção data, arquivo js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function pegaData() {
     let strDia = new String(dia)
     let strMes = new String(mes)
 
-    if (strDia.length == 1) mes = '0' + dia
+    if (strDia.length == 1) dia = '0' + dia
     if (strMes.length == 1) mes = '0' + mes
 
     switch (diaDaSemana) {
